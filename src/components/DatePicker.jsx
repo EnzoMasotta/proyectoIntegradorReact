a33a62277c194ce6444e8cc85b7230842cd958b8
EnzoMasotta <en.masotta20@hotmail.com>
2025-06-09 00:36:33 -0300

    Prueba de colores DatePicker

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -39,7 +39,7 @@ export function DatePicker() {
   const formatRange = (from, to) => {
     if (from && to)
       return `${format(from, "dd/MM/yyyy")} hasta ${format(to, "dd/MM/yyyy")}`;
-    if (from) return `Del ${format(from, "d MMM")}`;
+    if (from) return `${format(from, "dd/MM/yyyy")}`;
     return "";
   };
 
@@ -65,18 +65,36 @@ export function DatePicker() {
   };
 
   return (
-    <div className="relative w-full mt-4" ref={ref}>
-      <input
-        readOnly
-        value={formatRange(range.from, range.to)}
-        onClick={() => setOpen(true)}
-        placeholder="Selecciona fechas"
-        className="w-full border border-gray-400 text-gray-800 px-3 pt-6 pb-2 rounded-md focus:outline-none focus:border-blue-500"
-        style={{ paddingTop: "1.5rem" }}
-      />
-      <label className="absolute left-3 top-2 text-xs text-gray-500 pointer-events-none">
-        Fechas
-      </label>
+    <div className=" w-full mt-4" ref={ref}>
+      <div className="flex justify-between">
+        <div className="relative w-[48%]">
+          <input
+            readOnly
+            value={formatRange(range.from)}
+            onClick={() => setOpen(true)}
+            placeholder="Fecha de ida"
+            className="w-full border border-gray-400 text-gray-800 px-3 pt-6 pb-2 rounded-md focus:outline-none focus:border-blue-500"
+            style={{ paddingTop: "1.5rem" }}
+          />
+          <label className="absolute left-3 top-2 text-xs text-gray-500 pointer-events-none">
+            Ida
+          </label>
+        </div>
+
+        <div className="relative w-[48%]">
+          <input
+            readOnly
+            value={formatRange(range.to)}
+            onClick={() => setOpen(true)}
+            placeholder="Fecha de vuelta"
+            className="w-full border border-gray-400 text-gray-800 px-3 pt-6 pb-2 rounded-md focus:outline-none focus:border-blue-500"
+            style={{ paddingTop: "1.5rem" }}
+          />
+          <label className="absolute left-3 top-2 text-xs text-gray-500 pointer-events-none">
+            Vuelta
+          </label>
+        </div>
+      </div>
 
       {open &&
         (isMobile ? (
